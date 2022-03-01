db0751cf59972ce6e1aa422f245b45dd0208cd69
Tanvi Naik <naiktanvi30@gmail.com>
2022-03-01 13:01:34 +0530

    .gitignore is now working

--- projfrontend/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import Routers from "./Routers"
-import "./style.css"
-import "leaflet/dist/leaflet.css"
-import 'mapbox-gl/dist/mapbox-gl.css';
-
-ReactDOM.render(<Routers />, document.getElementById("root"));
