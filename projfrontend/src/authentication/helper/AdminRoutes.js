0977246ca926d1e36c22bfff485c0fd74a1d68a7
Tanvi Naik <67183737+TanvNaik@users.noreply.github.com>
2023-11-07 14:11:09 +0530

    Delete projfrontend directory

--- projfrontend/src/authentication/helper/AdminRoutes.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { Navigate, Outlet} from "react-router-dom";
-import {isAuthenticated} from "./index"
-
-const AdminRoute = () => {
-    return (isAuthenticated() && isAuthenticated().user.role == 1 ? <Outlet/> : <Navigate to={'/login'}/>)
-  }
-
-  export default AdminRoute
